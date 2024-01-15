f639b4086e46e4353aaaa27a2387dde230166d41
kazuki06kawamitu <138412466+kazuki06kawamitu@users.noreply.github.com>
2024-01-15 15:16:59 +0900

    ゲームジャム準備
    Player.cppまで完了

--- Game/DriveAndAvoid/Oblect/Barrier.h
+++ Game/DriveAndAvoid/Oblect/Barrier.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "../Utility/Vector2D.h"
+
+class barrier
+{
+private:
+	int image;   //バリア画像
+	float life_span;  //バリア寿命
+
+public:
+	Barrier();
+	~Barrier();
+
+	void Draw(const Vector2D& location);  //描画処理
+	bool IsFinished(float speed);
+};
